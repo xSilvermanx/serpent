a0076b564039895a4a5c4461a9bc78ca5b7eb9d9
xSilvermanx <62330001+xSilvermanx@users.noreply.github.com>
2020-10-10 18:02:08 +0200

    Init

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'multiple'
+description '[Serpent] Serverside Platform for Entity Handling - Dev Resource'
+version '0.1'
+
+server_scripts {
+  'entityhandler_sv.lua',
+  'nodes_sv.lua',
+  'paths_sv.lua'
+}
+
+client_scripts {
+  'entityhandler_cl.lua',
+  'paths_cl.lua',
+}
